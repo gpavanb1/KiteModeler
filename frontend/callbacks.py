b771af1e219e780ce4b688aaa301597eecf4133c
Pavan BG <gpbabc@yahoo.com>
2020-01-23 14:34:17 +0530

    values preserved between tabs

--- frontend/callbacks.py
+++ frontend/callbacks.py
@@ -1,28 +1,18 @@
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
-from .geometry import geometry_components
-from .bridle import bridle_components
-from .fly import fly_components
-
 from app import app
 
 
-@app.callback(Output('tabs-content', 'children'),
-              [Input('tabs', 'value')])
-def render_inputs(tab):
-    if tab == 'geometry':
-        return html.Div([
-            html.H3('Geometry'),
-            geometry_components
-        ])
-    elif tab == 'bridle':
-        return html.Div([
-            html.H3('Bridle'),
-            bridle_components
-        ])
-    elif tab == 'fly':
-        return html.Div([
-            html.H3('Fly'),
-            fly_components
-        ])
+@app.callback(Output('dashboard-content', 'children'),
+              [Input('h1', 'value'),
+               Input('h2', 'value'),
+               Input('w1', 'value'),
+               Input('b', 'value'),
+               Input('k', 'value'),
+               Input('wind_speed', 'value'),
+               Input('altitude', 'value'),
+               Input('line_length', 'value')
+               ])
+def update_inputs(**kwargs):
+    pass
--- index.py
+++ index.py
@@ -2,18 +2,31 @@ import dash_core_components as dcc
 import dash_html_components as html
 
 import frontend.callbacks
+from frontend.geometry import geometry_components
+from frontend.bridle import bridle_components
+from frontend.fly import fly_components
 
 from app import app
 
 
 app.layout = html.Div([
     html.H1(id='heading', children='Kite Modeler - Reboot'),
-    dcc.Tabs(id='tabs', value='geometry', children=[
-        dcc.Tab(label='Geometry', value='geometry'),
-        dcc.Tab(label='Bridle', value='bridle'),
-        dcc.Tab(label='Fly', value='fly')
-    ]),
-    html.Div(id='tabs-content')
+    dcc.Tabs([
+            dcc.Tab(label='Geometry', value='geometry', children=[html.Div([
+                html.H3('Geometry'),
+                geometry_components
+            ])]),
+            dcc.Tab(label='Bridle', value='bridle', children=[html.Div([
+                html.H3('Bridle'),
+                bridle_components
+            ])]),
+            dcc.Tab(label='Fly', value='fly', children=[html.Div([
+                html.H3('Fly'),
+                fly_components
+            ])])
+        ], id='tabs'),
+    html.Div(id='tabs-content'),
+    html.Div(id='dashboard-content')
 ])
 
 
